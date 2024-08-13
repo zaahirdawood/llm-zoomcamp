4517e360c9a3b5e5d889c586c0f763cfede64347
Zaahir Dawood <zaahirdawood@gmail.com>
2024-08-13 16:23:14 +0100

    updates to homework pipeline

---
+++ 05-orchestration/05-homework/rag-project/llm/rager/transformers/sublime_mecha.py
@@ -0,0 +1,39 @@
+import hashlib
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your transformation logic here
+
+    def generate_document_id(documents):
+        combined = f"{documents['course']}-{documents['question']}-{documents['text'][:10]}"
+        hash_object = hashlib.md5(combined.encode())
+        hash_hex = hash_object.hexdigest()
+        document_id = hash_hex[:8]
+        return document_id
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
